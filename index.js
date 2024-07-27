936656b0e7847018e8e0eabbd9bfd96a63f153dc
pourya <pourya@LAPTOP-FKIRP1VN>
2024-07-27 10:06:06 +0330

    add CSV File Handler

---
+++ index.js
@@ -0,0 +1,11 @@
+const { readCSV } =  require('./src/utils/fileHandler')
+// 1. Read CSV files    
+const readFiles = async() => {
+  const json = await readCSV('./input/sample.csv')
+  console.log('json',json) 
+}
+readFiles()
+// 2. Process Files and Merge Proudcts
+
+
+// 3. Generate output file
